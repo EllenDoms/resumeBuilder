4fa427ad40ac847cb755031ede8e1fed65910c0a
domsellen <domsellen@gmail.com>
2018-06-20 23:20:29 +0200

    New resume fix (when first resume)

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise';
 import reduxThunk from 'redux-thunk';
+import { browserHistory } from 'react-router';
 import { HashRouter, Route, Switch} from 'react-router-dom';
 import { fetchUser } from "../actions";
 import { connect } from "react-redux";
