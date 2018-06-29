bb5b1971d59737292dc062d123232a239bc660db
domsellen <domsellen@gmail.com>
2018-06-29 12:04:25 +0200

    loading page

--- src/components/loading.js
+++ src/components/loading.js
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 export default class Loading extends Component {
   render() {
     return (
-      <div id='page'>
-        Loading...
+      <div id='loadingPage' className="builderCss">
+        <div class="cs-loader">
+          <div class="cs-loader-inner">
+            <label>	●</label>
+            <label>	●</label>
+            <label>	●</label>
+            <label>	●</label>
+            <label>	●</label>
+            <label>	●</label>
+          </div>
+        </div>
       </div>
     );
   }
